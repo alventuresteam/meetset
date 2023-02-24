933075983b7404f40676092e06bea242975927df
Azer Mammadov <energon7777@gmail.com>
2023-02-24 09:42:47 +0400

    updates

--- routes/api.php
+++ routes/api.php
@@ -36,6 +36,7 @@ Route::group(['prefix' => 'reservations'], function() {
 
 Route::group(['prefix' => 'auth'], function() {
     Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
     Route::get('user', [AuthController::class, 'user'])->middleware('auth:sanctum');
 });
 
