13a45befebe4aa40528befa035ec6324cf898bfa
Azer Mammadov <energon7777@gmail.com>
2023-04-26 11:28:23 +0400

    updates

---
+++ app/Mail/SendReservation.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Attachment;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SendReservation extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private $ics;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($ics)
+    {
+        $this->ics = $ics;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Reservation',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return null;
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments(): array
+    {
+        return [
+          Attachment::fromData($this->ics->data,$this->ics->name)
+        ];
+    }
+}
