8b02f8eff12f9854df5f14d91541ec0d7fb3dace
Azer Mammadov <energon7777@gmail.com>
2023-08-24 14:18:07 +0400

    updates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Setting;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $setting = Setting::first();
+        Config::set('ldap.connections.default.hosts', $setting->ldap_host);
+        Config::set('ldap.connections.default.username', $setting->ldap_username);
+        Config::set('ldap.connections.default.base_dn', $setting->ldap_base_dn);
+        Config::set('ldap.connections.default.password', $setting->ldap_password);
+        Config::set('ldap.connections.default.port', $setting->ldap_port);
+        Config::set('ldap.connections.default.timeout', $setting->ldap_timeout);
     }
 }
