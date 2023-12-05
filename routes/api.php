b2b1cc251c6faac7d240d7e3bda1159f9864810a
Beycan <beycan.b@al.ventures>
2023-12-05 15:41:11 +0400

    ldap remove

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ImportController;
 use App\Http\Controllers\LogController;
 use App\Http\Controllers\MainInfoController;
